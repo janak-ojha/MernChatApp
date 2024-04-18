1b2d42c2baed5d6a6480840d7028e1b25e092afc
janak-ojha <janakrajojha66@gmail.com>
2024-04-18 19:44:53 +0530

    hello

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Login.css";
+
 
 const Login = () => {
+  const [email,setEmail] = useState();
+  const [password,setPassword] =useState();
+  const [show,setShow] = useState(false);
+  
+  const handleClick = () =>setShow(!show)
+
+  const submitHandler = () => {};
+  const submitHandler2 = () => {};
+
+
   return (
-    <div>Login</div>
+    <div className='Login'>
+      <form className='LoginForm'>
+        <label htmlFor='email' >Email</label>
+        <input type="email"
+        className='LoginInput'
+        id='email'
+        placeholder='Enter your email' 
+        required
+        onChange={(e) => setEmail(e.target.value)}
+        />
+        
+        <label htmlFor="password">Password</label>
+        <div className="paswordInputContainer">
+        <input
+          type={show ? "text" : "password"}
+          className='SignupInput'
+          id="password" // Add an ID to match the label's `for` attribute
+          placeholder='Enter your password'
+          required // Mark the input field as required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button style={{backgroundColor:"white",color:"black" , fontWeight:"600" }} 
+        type='button' onClick={handleClick}>
+          {show ? "hide" : "Show"}
+        </button>
+
+        </div>
+
+        <button  style={{backgroundColor:"blue", color:"white",padding:"8px" ,borderRadius:"10px",  marginTop:"15px"}} 
+        type="submit" onClick={submitHandler}>login</button>
+        <button  style={{backgroundColor:"red", color:"white" ,padding:"8px",borderRadius:"10px",  marginTop:"15px"}} 
+        type="submit" onClick={submitHandler2}>Get Guest User Credentials</button>
+         
+      </form>
+    </div>
   )
 }
 
--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,7 +1,24 @@
-import React from 'react';
-import './Signup.css'; // Import your CSS file for styling
+import React, { useState } from 'react';
+import './Signup.css';
+
+ // Import your CSS file for styling
 
 const Signup = () => {
+  const [show,setShow] = useState(false);
+  const [show1,setShow1] = useState(false);
+  const [name,setName] = useState();
+  const [email,setEmail] = useState();
+  const [password,setPassword] = useState();
+  const [confirmPassword,setConfirmPassword] = useState();
+  const [pic,setPic] = useState();
+  
+  const handleClick = () =>setShow(!show)
+  const handleClick1 = () =>setShow1(!show1)
+
+  const postDetails = (pics) => { };
+  const submitHandler = () => {};
+
+
   return (
     <div className='Signup'>
       <form className='SignupForm'>
@@ -12,6 +29,7 @@ const Signup = () => {
           id="name" // Add an ID to match the label's `for` attribute
           placeholder='Enter your name'
           required // Mark the input field as required
+          onChange={(e) => setName(e.target.value)}
         />
         <label htmlFor="email">Email</label>
         <input
@@ -20,26 +38,46 @@ const Signup = () => {
           id="email" // Add an ID to match the label's `for` attribute
           placeholder='Enter your email'
           required // Mark the input field as required
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label htmlFor="password">Password</label>
+        <div className="paswordInputContainer">
         <input
-          type="password"
+          type={show1 ? "text" : "password"}
           className='SignupInput'
           id="password" // Add an ID to match the label's `for` attribute
           placeholder='Enter your password'
           required // Mark the input field as required
+          onChange={(e) => setPassword(e.target.value)}
         />
+        <button style={{backgroundColor:"white",color:"black" , fontWeight:"600" }} 
+        type='button' onClick={handleClick1}>
+          {show1 ? "hide" : "Show"}
+        </button>
+
+        </div>
         <label htmlFor="confirmPassword">Confirm Password</label>
+        <div className="paswordInputContainer2">
         <input
-          type="password"
+          type={show ? "text" : "password"}
           className='SignupInput'
           id="confirmPassword" // Add an ID to match the label's `for` attribute
           placeholder='Confirm Password'
+          onChange={(e) => setConfirmPassword(e.target.value)}
           required // Mark the input field as required
         />
+        <button style={{backgroundColor:"white",color:"black",fontWeight:"600" }} 
+        type='button' onClick={handleClick}>
+          {show ? "hide" : "Show"}
+        </button>
+        </div>
         <label>Upload your picture (optional)</label> {/* Remove asterisk and colon */}
-        <input type="file" />
-        <button  type="submit">Sign Up</button>
+        <input type="file" 
+        id="pic"
+        onChange={(e) => postDetails(e.target.files[0])} />
+        <button  style={{backgroundColor:"blue", color:"white",padding:"6px" ,borderRadius:"10px",  marginTop:"15px"}} 
+        type="submit" onClick={submitHandler}>Sign Up</button>
+        
       </form>
     </div>
   );
