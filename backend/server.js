7863dd17375b90c766ae859fa6b0d7d687371eac
janak-ojha <janakrajojha66@gmail.com>
2024-06-11 00:03:06 +0530

    corrected

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ const PORT = process.env.PORT || 5000;
  const io = require("socket.io")(server,{
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: `${process.env.FRONTEND_URL}`,
     },
 
  });
